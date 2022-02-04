6e6772c3a3ffa68f6170ec1313f8feaa23d26ef4
QendresaZ <qz40092@ubt-uni.net>
2022-02-04 21:01:43 +0100

    first commit

---
+++ src/test/java/com/polarbookshop/catalogservice/bookservice/BookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polarbookshop.catalogservice.bookservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
